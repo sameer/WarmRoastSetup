b5c9f295f321ce006532e5a7fdd5ab0c9444216f
Robotia <spuri4096@gmail.com>
2016-02-17 20:36:43 -0500

    Add  to pass script args to WarmRoast

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 JHOME=`java -showversion -verbose 2>&1 | head -1`
 JHOME=`java chophome $JHOME`
 
-"$JHOME/bin/java" -Djava.library.path="$JHOME/jre/bin" -cp "$JHOME/lib/tools.jar:warmroast.jar" com.sk89q.warmroast.WarmRoast --thread "Server thread" -m ./conf
+"$JHOME/bin/java" -Djava.library.path="$JHOME/jre/bin" -cp "$JHOME/lib/tools.jar:warmroast.jar" com.sk89q.warmroast.WarmRoast --thread "Server thread" -m ./conf $@
